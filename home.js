2cad267135f3438216aa2cc65a2640574a48b8f5
Aldous Roy <aldousroy007@gmail.com>
2024-04-11 09:36:57 +0530

    first commit

---
+++ home.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const burger = document.querySelectorAll('.navbar-burger');
+    const menu = document.querySelectorAll('.navbar-menu');
+
+    if (burger.length && menu.length) {
+        for (var i = 0; i < burger.length; i++) {
+            burger[i].addEventListener('click', function() {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+
+    const close = document.querySelectorAll('.navbar-close');
+    const backdrop = document.querySelectorAll('.navbar-backdrop');
+
+    if (close.length) {
+        for (var i = 0; i < close.length; i++) {
+            close[i].addEventListener('click', function() {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+
+    if (backdrop.length) {
+        for (var i = 0; i < backdrop.length; i++) {
+            backdrop[i].addEventListener('click', function() {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+});
+var words = ['work', 'plan', 'grow', 'dream'];
+
+function getRandomWord() {
+    return words[Math.floor(Math.random() * words.length)];
+}
+
+// Function to update the content of the element with the random word
+function updateWord() {
+    var wordDisplay = document.getElementById('wordDisplay');
+    wordDisplay.textContent = getRandomWord();
+}
+
+// Call the updateWord function when the page loads
+window.onload = updateWord;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+const AniamteCSS = require( 'animated-tailwindcss');
+
+
+//module. exports = AniamteCSS({
+//content: ["•/src/**/*.{html,js}"],
+//theme: {
+//    extend: {},
+//    },
+plugins: [1, ],
+// })//
